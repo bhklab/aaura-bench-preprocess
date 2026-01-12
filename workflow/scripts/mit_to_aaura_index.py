d2da3d607a4e322fcb53ad0da2b4febd51cc8a1f
Katy <k.l.scott16@gmail.com>
2026-01-12 12:40:49 -0500

    feat: add multiple mask modality handling, dataset suffix and prefixes for handling OCSCC RADCURE

--- workflow/scripts/mit_to_aaura_index.py
+++ workflow/scripts/mit_to_aaura_index.py
@@ -16,16 +16,18 @@ logger = logging.getLogger(__name__)
 
 
 datasource = "TCIA"
-dataset = "HCC-TACE-Seg"
-ROI_key = "Mass"
+dataset = "RADCURE"
+ROI_key = "GTVp"
 image_modality = "CT"
-mask_modality = "SEG"
-lesion_location = "abdomen"
+mask_modality = ["RTSTRUCT"] # THIS HAS TO BE A LIST
+lesion_location = "headneck"
+special_prefix = "OCSCC_"
+special_suffix = "_windowed"
 
-dataset_path_prefix = Path(f"{datasource}_{dataset}/images/mit_{dataset}")
+dataset_path_prefix = Path(f"{datasource}_{dataset}/images/mit_{dataset}{special_suffix}")
 mit_dir_path = dirs.RAWDATA / dataset_path_prefix
 
-mit_index = pd.read_csv(mit_dir_path / f"mit_{dataset}_index-simple.csv")
+mit_index = pd.read_csv(mit_dir_path / f"mit_{special_prefix}{dataset}{special_suffix}_index-simple.csv")
 
 # Select out mask rows based on the ROI key specified
 mit_index_mask_rows = mit_index[mit_index['matched_rois'].str.contains(ROI_key, case=False, na=False)]
@@ -34,14 +36,26 @@ mit_index_mask_rows = mit_index[mit_index['matched_rois'].str.contains(ROI_key, 
 mit_index_image_rows = mit_index[mit_index['SeriesInstanceUID'].isin(mit_index_mask_rows['ReferencedSeriesUID'])]
 
 # Concatenate the image and mask rows into a single dataframe
-matched_index_rows = pd.concat([mit_index_mask_rows, mit_index_image_rows], ignore_index=True)
+selected_index_rows = pd.concat([mit_index_mask_rows, mit_index_image_rows], ignore_index=True)
 # Create a SampleID row for indexing purposes
-matched_index_rows = insert_SampleID(matched_index_rows)
-# Rearrange the dataframe so each row has a mask and image pair
-matched_index_rows = make_edges_df(matched_index_rows, 
-                              image_modality=image_modality,
-                              mask_modality=mask_modality)
+selected_index_rows = insert_SampleID(selected_index_rows)
 
+mod_dfs = {}
+for mod in mask_modality:
+    # Rearrange the dataframe so each row has a mask and image pair
+    modality_edges_df = make_edges_df(selected_index_rows, 
+                                image_modality=image_modality,
+                                mask_modality=str(mod))
+    mod_dfs[mod] = modality_edges_df
+
+# Combine the dataframes for each mask modality
+matched_index_rows = pd.concat(mod_dfs.values(), ignore_index=True)
+# Sort the index rows by SampleID
+matched_index_rows = matched_index_rows.sort_values('SampleID_image')
+
+if matched_index_rows.empty:
+    message = "No matching image and masks found with current settings. No aaura index to generate."
+    raise ValueError(message)
 
 # Get the columns out of mit_index needed in the aaura index
 aaura_columns_dict = {"id":matched_index_rows['SampleID_image'],
@@ -56,7 +70,7 @@ aaura_columns_dict = {"id":matched_index_rows['SampleID_image'],
 
 aaura_index = pd.DataFrame.from_dict(aaura_columns_dict)
 aaura_index['lesion_location'] = lesion_location
-aaura_index['source'] = dataset
+aaura_index['source'] = f"{special_prefix}{dataset}"
 aaura_index.insert(3, 'mask_idx', 1)
 aaura_index.insert(4, 'mask_voxel_label', 1)
 
@@ -75,4 +89,4 @@ aaura_index.insert(5, 'annotation_type', 'RERECIST')
 aaura_index.insert(6, 'annotation_coords', annotation_coords)
 aaura_index.insert(7, 'largest_slice_index', largest_slice_index)
 
-aaura_index.to_csv(mit_dir_path / f"aaura_{dataset}_index.csv", index=False)
+aaura_index.to_csv(mit_dir_path / f"aaura_{special_prefix}{dataset}{special_suffix}_index.csv", index=False)
